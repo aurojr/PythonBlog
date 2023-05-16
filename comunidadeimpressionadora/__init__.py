8eeb9398239ad50ba919ab82dbf60ee680e10550
Auro de Oliveira Junior <auroojr@gmail.com>
2023-05-16 15:42:52 -0300

    Correção BD - init4

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-import sqlalchemy as sa
+import sqlalchemy
 from sqlalchemy import inspect, Inspector
 
 app = Flask(__name__)
@@ -24,9 +24,9 @@ login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import models
 
-engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspector = sa.inspect(engine)
-if not sa.engine.reflection.Inspector.has_table(inspector, 'usuario'):
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
     with app.app_context():
         database.drop_all()
         database.create_all()
