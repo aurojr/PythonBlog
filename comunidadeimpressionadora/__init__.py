e66f72bf7e6e24cab8823c43bec0e8fb3e008505
Auro de Oliveira Junior <auroojr@gmail.com>
2023-05-16 15:30:48 -0300

    Correção BD - init2

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,13 +4,13 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 import sqlalchemy
-from sqlalchemy import inspect, Inspector, inspection
+from sqlalchemy import inspect, Inspector
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:BvKCN0reD0AVyg89rf2f@containers-us-west-16.railway.app:5488/railway'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
@@ -26,7 +26,7 @@ from comunidadeimpressionadora import models
 
 engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 inspector = sqlalchemy.inspect(engine)
-if not sqlalchemy.engine.reflection.Inspector.has_table(inspection, "usuario"):
+if not inspector.has_table('usuario'):
     with app.app_context():
         database.drop_all()
         database.create_all()
